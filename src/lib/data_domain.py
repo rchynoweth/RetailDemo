37310a0d490a5e4698d893eb19aaf4d2e07ee67c
rchynoweth <79483287+rchynoweth@users.noreply.github.com>
2022-09-09 14:33:26 -0700

    Create data_domain.py

---
+++ src/lib/data_domain.py
@@ -0,0 +1,38 @@
+import random
+
+
+class DataDomain():
+  
+  __instance = None
+
+  @staticmethod
+  def getInstance():
+      """ Static access method. """
+      if DataDomain.__instance == None:
+          DataDomain()
+      return DataDomain.__instance 
+    
+  
+  def __init__(self):
+    self.store_ids = [] 
+    self.customer_ids = []
+    self.products = None 
+    
+    
+  def select_random_customer(self):
+    return self.customer_ids[random.randint(0, len(self.customer_ids)-1)]
+
+  
+  def select_random_store(self):
+    return self.store_ids[random.randint(0, len(self.store_ids)-1)]
+  
+  
+  def add_customer(self, cid):
+    self.customer_ids.append(cid)
+    
+  
+  def add_store(self, sid):
+    self.store_ids.append(sid)  
+    
+  def set_products(self, products):
+    self.products = products
